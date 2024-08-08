47283ccb0698445df42485856875baab598b849a
OluC94 <coker.olu94@gmail.com>
2024-08-08 15:05:12 +0100

    TennisGame3 finish changes

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 - Class fields
   - Rearranged declaration for p1 and p2 for consistency 
   - Rename for clarity (p1 --> p1score, p1N --> p1Name)
+  - made name variables final because they do not change
 - Get score function
-  - rename s to scoreOutput
+  - extract the function that determines the score output while the game is active
+  - Rearrange the function so that the if conditions are no longer nested
 - Won point function
   - Changed conditional to use Object.equals()
+  - removed unnecessary "this" keyword
--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -4,8 +4,8 @@ public class TennisGame3 implements TennisGame {
 
     private int p1score;
     private int p2score;
-    private String p1Name;
-    private String p2Name;
+    private final String p1Name;
+    private final String p2Name;
 
     public TennisGame3(String p1Name, String p2Name) {
         this.p1Name = p1Name;
@@ -13,25 +13,42 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String scoreOutput;
-        if (p1score < 4 && p2score < 4 && !(p1score + p2score == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            scoreOutput = p[p1score];
-            return (p1score == p2score) ? scoreOutput + "-All" : scoreOutput + "-" + p[p2score];
-        } else {
-            if (p1score == p2score)
-                return "Deuce";
-            scoreOutput = p1score > p2score ? p1Name : p2Name;
-            return ((p1score-p2score)*(p1score-p2score) == 1) ? "Advantage " + scoreOutput : "Win for " + scoreOutput;
+        boolean isGamePreDeuce = p1score < 4 && p2score < 4 && !(p1score + p2score == 6);
+
+        if (isGamePreDeuce) {
+            return determineScoreOutput();
         }
+
+        if (p1score == p2score) {
+            return "Deuce";
+        }
+
+        String leadingPlayer = p1score > p2score ? p1Name : p2Name;
+
+        boolean isAdvantage = (p1score-p2score)*(p1score-p2score) == 1;
+
+        return isAdvantage ? "Advantage " + leadingPlayer : "Win for " + leadingPlayer;
+
     }
-    
+
+    private String determineScoreOutput(){
+        String[] possibleScores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        String p1ScoreString = possibleScores[p1score];
+        String p2ScoreString = possibleScores[p2score];
+
+        boolean isScoreLevel = p1score == p2score;
+        String levelScores = p1ScoreString + "-All";
+        String nonLevelScores = p1ScoreString + "-" + p2ScoreString;
+
+        return isScoreLevel ? levelScores : nonLevelScores;
+    }
+
+
     public void wonPoint(String playerName) {
         if (Objects.equals(playerName, "player1"))
-            this.p1score += 1;
+            p1score++;
         else
-            this.p2score += 1;
-        
+            p2score++;
     }
 
 }
